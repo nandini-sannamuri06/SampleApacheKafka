988fbe3cea1948a9f2df0eeb148d93aa87be3188
nandini-sannamuri06 <suggula.nandini@gmail.com>
2019-03-05 14:59:04 +0530

    Update KafkaController.java

--- src/main/java/com/sample/kafka/controller/KafkaController.java
+++ src/main/java/com/sample/kafka/controller/KafkaController.java
@@ -1,14 +1,20 @@
 package com.sample.kafka.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.sample.kafka.ItemDetails;
+import com.sample.kafka.Repository.ItemDetailsRepository;
 
 @RestController
 public class KafkaController {
@@ -16,11 +22,28 @@ public class KafkaController {
 	@Autowired
 	KafkaTemplate<String, String> kafkaTemplate;
 	
+	@Autowired
+	ItemDetailsRepository itemDetailsRepository;
+	
 	
 	@PostMapping(value="/sender")
 	public ResponseEntity<String> sendData(@RequestBody String data) {
 		kafkaTemplate.send("SAMPLE_TEST_TOPIC", data.toString());
 		return new ResponseEntity<String>("Data sent successfully",HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/fetchDetails")
+	public ResponseEntity<String> fetchData(@RequestParam Integer itemId) {
+		Optional<ItemDetails> itemDetails = itemDetailsRepository.findByItemId(itemId);
+		if(itemDetails.isPresent()) {
+		String data = new Gson().toJson(itemDetails);
+		kafkaTemplate.send("SAMPLE_RECEIVE_TOPIC", data);
+		return new ResponseEntity<>("Data successfully sent",HttpStatus.OK);
+		}else {
+			System.out.println("Item Details are not available");
+			return new ResponseEntity<String>("Item Details are not available",HttpStatus.BAD_REQUEST);
+		}
+		
 	}
 	
 
