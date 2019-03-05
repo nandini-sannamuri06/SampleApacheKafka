9dc8c441e31d5a27ee5081d808bd9a79760dd282
nandini-sannamuri06 <suggula.nandini@gmail.com>
2019-03-05 13:50:31 +0530

    Update KafkaListner.java

--- src/main/java/com/sample/kafka/KafkaListner.java
+++ src/main/java/com/sample/kafka/KafkaListner.java
@@ -5,17 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.google.gson.Gson;
 import com.sample.kafka.Repository.ItemDetailsRepository;
 
 @Component
 public class KafkaListner {
 	 
 	@Autowired
-	ItemDetailsRepository ItemDetailsRepository;
+	ItemDetailsRepository itemDetailsRepository;
 	
 	 @KafkaListener(topics= {"SAMPLE_TEST_TOPIC"}, groupId = "group-id")
 	    public void listenPartition0(ConsumerRecord<?, ?> record) {
-	        System.out.println("Received: " + record);
-	        //ItemDetailsRepository.save(record.value());
+	        System.out.println("Received: " + record.value());
+	        itemDetailsRepository.save(new Gson().fromJson(record.value().toString(),ItemDetails.class));
 	    }
 }
